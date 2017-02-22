ebff294c71b7b8bac21087ade63ff1b4facdd338
Octa <Octa@Octavio-HP>
2017-02-22 19:16:54 -0300

    Equals

--- Ayudarg/src/main/java/com/ayudarg/model/InstitucionSQL.java
+++ Ayudarg/src/main/java/com/ayudarg/model/InstitucionSQL.java
@@ -20,14 +20,14 @@ import javax.persistence.Table;
 import com.ayudar.elasticsearch.model.Usuario;
 
 @Entity
-@Table(name="Institucion")
+@Table(name = "Institucion")
 
 public class InstitucionSQL {
 
 	@Id
-	@Column(name="idInstitucion")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	
+	@Column(name = "idInstitucion")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private int idInstitucion;
 	private String director;
 	private String tipo;
@@ -38,29 +38,29 @@ public class InstitucionSQL {
 	private String sitioWeb;
 	private String email;
 	private boolean activo;
-	
-    @ManyToOne(fetch = FetchType.LAZY)
+
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "localidadesId", nullable = false)
-    private LocalidadesSQL localidadesId;
-	
+	private LocalidadesSQL localidadesId;
+
 	@ManyToMany(cascade = CascadeType.ALL)
-	@JoinTable(name = "InstitucionHasCategoria", 
-	    joinColumns = { @JoinColumn(name = "institucionIdInstitucion") }, 
-	    inverseJoinColumns = { @JoinColumn(name = "categoriaIdCategoria") })
+	@JoinTable(name = "InstitucionHasCategoria", joinColumns = {
+			@JoinColumn(name = "institucionIdInstitucion") }, inverseJoinColumns = {
+					@JoinColumn(name = "categoriaIdCategoria") })
 	private Set<Categoria> categoria = new HashSet<Categoria>(0);
-		
+
 	public Set<Categoria> getCategoria() {
 		return categoria;
 	}
-	
+
 	public void setCategoria(Set<Categoria> categoria) {
 		this.categoria = categoria;
 	}
-	
-    @ManyToOne(fetch = FetchType.LAZY)
+
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "idUsuario", nullable = false)
 	private UsuarioSQL idUsuario;
-    
+
 	public UsuarioSQL getIdUsuario() {
 		return idUsuario;
 	}
@@ -70,9 +70,9 @@ public class InstitucionSQL {
 	}
 
 	@OneToMany(cascade = CascadeType.ALL)
-    private Set<RecursoSQL> recurso = new HashSet<RecursoSQL>(0);
-    
-    public LocalidadesSQL getLocalidadesId() {
+	private Set<RecursoSQL> recurso = new HashSet<RecursoSQL>(0);
+
+	public LocalidadesSQL getLocalidadesId() {
 		return localidadesId;
 	}
 
@@ -83,69 +83,99 @@ public class InstitucionSQL {
 	public boolean getActivo() {
 		return activo;
 	}
+
 	public void setActivo(boolean activo) {
 		this.activo = activo;
 	}
-    public int getIdInstitucion() {
+
+	public int getIdInstitucion() {
 		return idInstitucion;
 	}
+
 	public void setIdInstitucion(int idInstitucion) {
 		this.idInstitucion = idInstitucion;
 	}
+
 	public String getDirector() {
 		return director;
 	}
+
 	public void setDirector(String director) {
 		this.director = director;
 	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getDireccion() {
 		return direccion;
 	}
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+
 	public String getTelefono() {
 		return telefono;
 	}
+
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
+
 	public String getCelular() {
 		return celular;
 	}
+
 	public void setCelular(String celular) {
 		this.celular = celular;
 	}
+
 	public String getSitioWeb() {
 		return sitioWeb;
 	}
+
 	public void setSitioWeb(String sitioWeb) {
 		this.sitioWeb = sitioWeb;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Set<RecursoSQL> getRecursos() {
 		return this.recurso;
 	}
+
 	public void setRecurso(Set<RecursoSQL> recurso) {
 		this.recurso = recurso;
 	}
+
+	@Override
+	 public boolean equals(Object obj) {
+        if ( obj == null ) return false;
+        if ( this == obj ) return true;
+        if ( ! (obj instanceof InstitucionSQL ) ) return false;
+        InstitucionSQL i = (InstitucionSQL) obj;
+        return this.idInstitucion == i.idInstitucion ;
+    }	
 	
 	@Override
 	public String toString() {
